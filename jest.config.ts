ddd320bdc479daae85a9e296142f3ca319db9efe
Son Quang Pham <phamquangson4493@gmail.com>
2026-09-14 12:49:53 +0700

    ci: add workflow and fix jest test separation

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/', '/tests-examples/'],
+};
+
+export default config;
---
+++ src/__tests__/sanity.test.ts
@@ -0,0 +1,5 @@
+describe('sanity check', () => {
+  it('runs a basic unit test', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
